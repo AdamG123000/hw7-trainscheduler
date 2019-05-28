a69f385e63bb8b9ef6de25de61e0642c6003a84a
Adam Grodahl <adamgrodahl@Adams-MBP.hsd1.or.comcast.net>
2019-05-28 12:44:14 -0700

    first commit

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBJ9oytKN5Mvs5LRUZs3iTOq6uP8NPoUkQ",
+    authDomain: "hw7-firebase-24a54.firebaseapp.com",
+    databaseURL: "https://hw7-firebase-24a54.firebaseio.com",
+    projectId: "hw7-firebase-24a54",
+    storageBucket: "hw7-firebase-24a54.appspot.com",
+    messagingSenderId: "630173090412",
+    appId: "1:630173090412:web:50861adb35616ea8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+})
