03e1bb423503568f99169d3cab97faf1e83a3e7c
Adam Grodahl <adamgrodahl@Adams-MacBook-Pro.local>
2019-05-28 17:36:40 -0700

    progress

--- assets/app.js
+++ assets/app.js
@@ -14,3 +14,44 @@ $(document).ready(function(){
   firebase.initializeApp(firebaseConfig);
 
 })
+
+//variable to store the database info 
+var database = firebase.database();
+
+//variables 
+var trainName = "";
+var trainDestination = "";
+var trainFrequency = 0;
+var trainTime = "";
+var clickCounter = 1;
+
+//Capturing the add train button click 
+
+$("#add-train").on("click", function(event){
+    event.preventDefault();
+    if ($("#train-input").val() > 24) {
+        alert("There are only 24 hours in a day");
+    } else {
+        //variables that will hold the user input values
+        trainName = $("#train-input").val().trim();
+        trainDestination = $("#destination-input").val().trim();
+        trainTime = $("#train-input").val().trim();
+        trainFrequency = $("#frequency-input").val().trim();
+
+        //console.log to see if the variables are holding the user input values
+        console.log("Input Values");
+        console.log(trainName);
+        console.log(trainDestination);
+        console.log(trainTime);
+        console.log(trainFrequency);
+
+        //Local object for holding train details
+        var trainDetail = {
+            name : trainName,
+            destination : trainDestination,
+            frequency : trainFrequency,
+            time : trainTime
+        }
+
+    };
+})
